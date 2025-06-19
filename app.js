088d30e01df32ca31d1d5db6b9df5f7ba120a9c9
KvngOLA <davidskenneth01@gmail.com>
2025-06-19 15:28:04 +0100

    Added a helmet and RateLimiter

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userRouter = require("./router/router");
 const postRouter = require("./router/post.routes");
 const adminRouter = require("./router/admin.routes");
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const remindInactiveUsers = require("./reminder/reminder");
 const deleteInactiveUsers = require("./scheduler/scheduler");
 
@@ -14,6 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 10,
+  standardHeaders: "draft- 7",
+  legacyHeaders: false,
+});
+app.use(limiter);
+app.use(helmet());
+
 remindInactiveUsers();
 deleteInactiveUsers();
 
@@ -22,8 +33,12 @@ app.use("/v1/post", postRouter);
 app.use("/v1/admin", adminRouter);
 
 app.use((req, res) => {
-    return res.status(404).json({ message: `The resourse with the url::: ${req.originalUrl} does not exist`})
-})
+  return res
+    .status(404)
+    .json({
+      message: `The resourse with the url::: ${req.originalUrl} does not exist`,
+    });
+});
 app.listen(port, () => console.log(`Listening to requests on port ${port}`));
 
 connectDb();
